6a2e4d63944e878d26ba5405a0e9f33a3f7a173a
Long Huynh <longht021189@gmail.com>
2023-08-21 14:38:08 +0700

    update gen script

--- template.ts
+++ template.ts
@@ -1,16 +1,14 @@
 import { By, until } from 'selenium-webdriver';
 import { getDriver } from './src/driver';
 import { getRemoteUrl } from './src/input';
-import { clickGoogleItem, searchWithGoogle, visitWeb } from './src/actions';
+import * as actions from './src/actions';
 
 async function main() {
   const remoteUrl = getRemoteUrl();
   const driver = getDriver(remoteUrl);
 
   try {
-    await visitWeb(driver, 'http://www.google.com/');
-    await searchWithGoogle(driver, 'Nike,Adiddas,Sephora');
-    await clickGoogleItem(driver, '0');
+    // TODO
   } catch(error: any) {
     console.log('ERROR', error);
   } finally {
